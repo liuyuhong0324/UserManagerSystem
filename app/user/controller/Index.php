e91722bacf649d7b6547860a280f9487b976fa62
liuyuhong0324 <liuyuhong0324@gmail.com>
2019-12-13 16:17:02 +0800

    完善修改用户信息

--- app/user/controller/Index.php
+++ app/user/controller/Index.php
@@ -24,4 +24,27 @@ class Index extends Controller
             return $this->error("您还未登录,请登录!",'/user/login/index');
         }
     }
+
+    function edit($username)
+    {
+        $session = session('user');
+        //判断用户是否登录,后续使用公共类继承解决,不用每次判断
+        if($session)
+        {
+            //session('logined',request()->url());
+            $user = new User();
+            $user = $user -> where('username',$username);
+            $list = $user -> paginate();
+            return view('edit',['list' => $list]);
+        }
+        else
+        {
+            return $this->error("您还未登录,请登录!",'/user/login/index');
+        }
+    }
+
+    function delete($username)
+    {
+        return view();
+    }
 }
